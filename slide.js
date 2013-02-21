f7cf64d902cb91c099789c689f1453c74eaaf488
etp1711 <etp1711@gmail.com>
2013-02-21 14:06:47 +0330

    edit my pic

--- slide.js
+++ slide.js
@@ -3,18 +3,18 @@ window.onload = function(){
 	var slide = document.getElementsByClassName('slide').item(0);
 	var slidebar = slide.getElementsByClassName('slidebar').item(0);
 	var train = slidebar.getElementsByClassName('train').item(0);
-	var img = train.getElementsByClassName('img');
+	var img = train.getElementsByTagName('img');
 	
 	var pre = slide.getElementsByClassName('previous').item(0); 
 	var next = slide.getElementsByClassName('next').item(0); 
 	var currentslide = 0;
 	
 	slider = function(n){
-		if(n>(img.length-6)) n=0;
-		if(n<0) n = (img.length-6);
-		train.style.left = -(150) + 'px';
+		if(n>(img.length-1)) n=0;
+		if(n<0) n = (img.length-1);
+		train.style.left = (-157*n)+'px';
 		currentslide = n;
-	}
+	};
 	
 	nextslide = function(){
 		slider(currentslide+1);
@@ -24,11 +24,11 @@ window.onload = function(){
 		slider(currentslide-1);
 	}
 	
-	var intervalName = false;
+	/*var intervalName = false;     b nazaram in ghesmat bikhode.
 	(startSliding = function(){
 		if(intervalName) return; 
 		intervalName = setInterval(nextslide,3000);
-	})();
+	})();*/
 	
 	stopSliding = function(){
 		if(!intervalName) return;
@@ -37,11 +37,6 @@ window.onload = function(){
 	}
 	
 	next.onclick = nextslide;
-	previous.onclick = preslide;
-	
-	slideshow.onmouseover = stopSliding;
-	slideshow.onmouseout = startSliding;
-	
-	slider(0);
+	pre.onclick = preslide;	
 	
 }

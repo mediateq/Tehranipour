f4d92465d4b5d06e284197a2af2cd1acb1ab74f9
etp1711 <etp1711@gmail.com>
2013-03-19 01:42:48 +0430

    edtt ex

--- gallery.php
+++ gallery.php
@@ -4,53 +4,53 @@
 		<div class="gallery" id="loader">
 			<ul>
 				<li>
-					<a href="./images/gallery/1.jpg" rel="prettyphoto[gallery1]" title="کوهسنجی">
+					<a href="./images/gallery/1.jpg" rel="prettyphoto[gallery1]" title="">
 						<img src="./images/gallery/1.jpg" alt="">
 						<div class="effe"></div>
 					</a>
-					<p>کوهسنجی</p>
+					<p></p>
 				</li>
 				<li>
-					<a href="./images/gallery/2.jpg" rel='prettyphoto[gallery1]' title="مجمع بروما">
+					<a href="./images/gallery/2.jpg" rel='prettyphoto[gallery1]' title="">
 						<img src="./images/gallery/2.jpg" alt="">
 						<div class="effe"></div>
 					</a>
-					<p>مجمع بروما</p>
+					<p></p>
 				</li>
 				<li>
-					<a href="./images/gallery/3.jpg" rel='prettyphoto[gallery1]' title="اخلمد">
+					<a href="./images/gallery/3.jpg" rel='prettyphoto[gallery1]' title="">
 						<img src="./images/gallery/3.jpg" alt="">
 						<div class="effe"></div>
 					</a>
-					<p>اخلمد</p>
+					<p></p>
 				</li>
 				<li>
-					<a href="./images/gallery/4.jpg" rel='prettyphoto[gallery1]' title="ضریح الامام رضا(ع)">
+					<a href="./images/gallery/4.jpg" rel='prettyphoto[gallery1]' title="">
 						<img src="./images/gallery/4.jpg" alt="">
 						<div class="effe"></div>
 					</a>
-					<p>ضریح الامام رضا(ع)</p>
+					<p></p>
 				</li>
 				<li>
-					<a href="./images/gallery/5.jpg" rel='prettyphoto[gallery1]' title="حديقة ملت">
+					<a href="./images/gallery/5.jpg" rel='prettyphoto[gallery1]' title="">
 						<img src="./images/gallery/5.jpg" alt="" >
 						<div class="effe"></div>
 					</a>
-					<p>حديقة ملت</p>
+					<p></p>
 				</li>
 				<li>
-					<a href="./images/gallery/6.jpg" rel='prettyphoto[gallery1]' title="طرقبه و شاندیز">
+					<a href="./images/gallery/6.jpg" rel='prettyphoto[gallery1]' title="">
 						<img src="./images/gallery/6.jpg" alt="">
 						<div class="effe"></div>
 					</a>
-					<p>طرقبه و شاندیز</p>
+					<p></p>
 				</li>
 				<li>
-					<a href="./images/gallery/7.jpg" rel='prettyphoto[gallery1]' title="مسبح سرزمین آفتاب">
+					<a href="./images/gallery/7.jpg" rel='prettyphoto[gallery1]' title="">
 						<img src="./images/gallery/7.jpg" alt="">
 						<div class="effe"></div>
 					</a>
-					<p>مسبح سرزمین آفتاب</p>
+					<p></p>
 				</li>
 			</ul>
 			<div class="badboy"></div>
